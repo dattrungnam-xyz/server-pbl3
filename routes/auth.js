b9239f8c91b7f10ac772001b3bf0212f0e9c32ca
dattrungnam <e29bechanh@email.com>
2023-03-21 19:27:41 +0700

    test

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+import authController from "../controllers/authController.js";
+
+import express from 'express';
+
+
+
+const router = express.Router();
+
+
+
+router.post("/register", authController.registerUser);
+
+router.post("/login", authController.loginUser);
+
+
+export default router;
